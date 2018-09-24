46d46cfd1e704f41bf4d8f49dd7d633aca9a3d48
LydiaNWBot <43548875+LydiaNWBot@users.noreply.github.com>
2018-09-24 20:58:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -85,7 +85,8 @@ client.on('message', msg => {
 			msg.author.sendMessage('**Aleyküm Selam Sunucuya Hoş Geldin**'); 
 		} else {
 		msg.reply('**Aleyküm Selam Sunucuya Hoş Geldin**');
-	         msg.react(':smile:');
+	        .then(function (message) {
+          message.react("👍")
 		}
 	}
 });
