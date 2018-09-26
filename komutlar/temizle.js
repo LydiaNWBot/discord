9f83529a8981a8aa33406047533a9183b1e29fc5
LydiaNWBot <43548875+LydiaNWBot@users.noreply.github.com>
2018-09-26 17:18:54 +0300

    Update temizle.js

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -1,19 +1,26 @@
 const Discord = require('discord.js');
 
 
-exports.run = function(client, message) {
-message.channel.bulkDelete(20);
-message.channel.send("20 mesaj sildim").then(msg => {
-	msg.delete(5000)
-})
+exports.run = function(client, message, args) {
+  const sayi = args.slice(0).join(' ');
+
+
+  if(sayi.length < 1) {
+    return message.reply("Silmem için bir miktar belirt")
+  } else {
+        message.channel.bulkDelete(sayi + 1);
+    message.channel.send("**" + sayi + "** adet mesaj sildim!").then(msg =>{
+        msg.delete("5000")
+    });
+  }
 
 };
 
 exports.conf = {
   enabled: true, 
   guildOnly: false, 
-  aliases: [],
-  permLevel: 0 
+  aliases: ['sil'],
+  permLevel: 2 
 };
 
 exports.help = {
